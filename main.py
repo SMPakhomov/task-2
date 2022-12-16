679507a6c18e57f5cad6de9ec68d1e06c42d8142
Семён <sem.pakhomov2007@yandex.ru>
2022-12-16 18:12:17 +0300

    rand color

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ MYEVENTTYPE = pygame.USEREVENT + 1
 pygame.time.set_timer(MYEVENTTYPE, 10)
 
 
-def draw(screen, pos, r):
-    pygame.draw.circle(screen, pygame.Color("yellow"), pos, r)
+def draw(screen, pos, r, color):
+    pygame.draw.circle(screen, color, pos, r)
 
 
 pygame.init()
@@ -17,6 +17,7 @@ running = True
 screen = pygame.display.set_mode(size)
 screen.fill(pygame.Color("black"))
 pos = (random.randint(1, 600), random.randint(0, 600))
+color = (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))
 r = random.randint(1, 50)
 while running:
     for event in pygame.event.get():
@@ -29,7 +30,8 @@ while running:
             pos = event.pos
             pygame.time.set_timer(MYEVENTTYPE, 10)
         if event.type == MYEVENTTYPE:
-            draw(screen, pos, r)
+            draw(screen, pos, r, color)
             pos = (random.randint(1, 600), random.randint(0, 600))
             r = random.randint(1, 50)
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     pygame.display.flip()
